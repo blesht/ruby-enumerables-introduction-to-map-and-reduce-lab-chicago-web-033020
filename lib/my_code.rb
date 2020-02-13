f89d813fbcc89783d4502f242dc7110ca0ad224c
bryan lesht <bryan.lesht@colorado.edu>
2020-02-13 23:01:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,34 @@
 # My Code here....
+def map_to_negativize(array)
+  i = 0 
+  while i < array.length do 
+    array[i] *= -1
+    i += 1 
+  end 
+  array 
+end 
+
+
+def map_to_no_change(array)
+  array 
+end
+
+def map_to_double(array)
+  i = 0 
+  while i < array.length do 
+    array[i] *= 2 
+    i += 1 
+  end 
+  array
+end
+
+def map_to_square(array)
+  i = 0 
+  while i < array.length do 
+    array[i] **= 2 
+    i += 1 
+  end 
+  array 
+end
+
+
